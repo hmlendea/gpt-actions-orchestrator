3fbff59f340cff7169761d3b6ec01be1824ee0b0
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-03-15 22:05:17 +0200

    github: Retrieve more info about GitHub repositories

--- Integrations/GitHub/Service/Models/GitHubRepository.cs
+++ Integrations/GitHub/Service/Models/GitHubRepository.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace GptActionsOrchestrator.Integrations.GitHub.Service.Models
@@ -7,10 +9,31 @@ namespace GptActionsOrchestrator.Integrations.GitHub.Service.Models
         [JsonPropertyName("name")]
         public string Name { get; set; }
 
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("language")]
+        public string Language { get; set; }
+
+        [JsonPropertyName("stargazers_count")]
+        public int StargazersCount { get; set; }
+
+        [JsonPropertyName("topics")]
+        public IReadOnlyCollection<string> Topics { get; set; }
+
         [JsonPropertyName("archived")]
         public bool IsArchived { get; set; }
 
         [JsonPropertyName("private")]
         public bool IsPrivate { get; set; }
+
+        [JsonPropertyName("fork")]
+        public bool IsFork { get; set; }
+
+        [JsonPropertyName("created_at")]
+        public DateTimeOffset CreatedAt { get; set; }
+
+        [JsonPropertyName("pushed_at")]
+        public DateTimeOffset PushedAt { get; set; }
     }
 }
